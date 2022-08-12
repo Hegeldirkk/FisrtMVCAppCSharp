f83e360df3a5b1d7491dcffd6f1a273a659ce19d
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-12 00:51:49 +0000

    first commit

---
+++ FisrtMVCAppCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FisrtMVCAppCSharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
